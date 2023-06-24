c52d89a92b1196bc4974d3792e85586ca54a4964
Aishat Akinyemi <akinyemiomolola99@gmail.com>
2023-06-24 16:57:21 +0100

    Little fixings.

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -58,33 +58,33 @@ const Comments = () => {
                 </span>
               </div>
             </div>
-            <div className="text-[18px]  rounded-[3px] text-moderateBlue absolute lg:left-2 lg:top-5 bottom-2  lg:ml-0  bg-lightGray lg:w-[20px] w-[55px] flex lg:flex-col justify-center items-centers lg:gap-2 gap-2 lg:h-[60px] h-6">
+            <div className="text-[18px]  rounded-[3px] text-moderateBlue absolute lg:left-2 lg:top-5 bottom-2  lg:ml-0  bg-lightGray lg:w-[20px] w-[55px] flex lg:flex-col justify-center items-center lg:gap-2 gap-2 lg:h-[60px] h-6">
               <svg
                 width="11"
                 height="11"
                 xmlns="http://www.w3.org/2000/svg"
                 onClick={() => dispatch(upVoteOtherComment(item.id))}
-                className="self-center cursor-pointer "
+               
               >
                 <path
-                  className="transition-all duration-300 delay-100 hover:fill-moderateBlue"
+                  className="transition-  cursor-pointer duration-300 delay-100 hover:fill-moderateBlue"
                   d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
                   fill="#C5C6EF"
                 />
               </svg>
-              <span className="text-[10px] font-[700] self-center">
+              <span className="text-[12px] font-[700] self-center">
                 {item.score}
               </span>
 
               <svg
-                className="self-center cursor-pointer "
+              
                 width="11"
                 height="3"
                 xmlns="http://www.w3.org/2000/svg"
                 onClick={() => dispatch(downVoteOtherComment(item.id))}
               >
                 <path
-                  className="transition-all duration-300 delay-100 hover:fill-moderateBlue"
+                  className="transition-all  cursor-pointer duration-300 delay-100 hover:fill-moderateBlue"
                   d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
                   fill="#C5C6EF"
                 />
